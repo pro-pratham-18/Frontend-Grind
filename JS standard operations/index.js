940aa2fc592f6dde4d49e343642b130b08eeed90
pro-pratham-18 <itz.prathathamesh18@gmail.com>
2026-02-02 22:59:27 +0530

    javasript learned

---
+++ JS standard operations/index.js
@@ -0,0 +1,82 @@
+// {
+//     let a=10;
+//     console.log(a);
+// }
+// console.log(a); 
+// let x ="Prathamesh Joshi";
+// console.log(typeof(x));
+
+// let a=Symbol("id");
+// let b=Symbol("id");
+// const c=Symbol(56);
+// console.log(typeof(a));
+// console.log(a);
+// console.log(c); 
+
+// let a='prathamesh';
+// console.log(typeof(a));
+
+// let a=50;
+// let b="50";
+// console.log(a==b);                    True
+// console.log(a===b);                   False
+
+// let a=Number(prompt("Enter the Number"));
+// console.log(a*2);
+
+// const readline =require("readline");
+// console.log(readline);
+ 
+// let age=20;   
+// (age>18)?console.log("you can vote"):console.log("you cannnot vote");  ternary operator
+
+//logical op same as cpp
+
+
+// bitwise and or not xor , & | ~ 
+
+// left shift << 
+// right shift >>
+
+// console.log(5<<2);       multiply by 2 ki power 2
+// console.log(16>>2);      divide by 2 ki power 2
+
+// let age=76;
+
+
+// conditionals in js
+
+// if(age>=18 && age<45 ){
+//     console.log("you can vote")
+// }
+// else if(age>=45){
+//     console.log("you are uncle,ofc you can vote")
+// }
+// else{
+//     console.log("you can't vote")
+// }
+
+
+// switch case in js
+
+// let a = 2;
+// switch(a){
+//     case 1:console.log('A');
+//     break;
+//     case 2:console.log('B');
+//     break;
+//     case 3:console.log('C');
+//     break;
+//     default: console.log('D');
+// }
+
+
+
+
+
+
+
+
+
+
+ 
